73b658b18b5dbb266869f00350fd69d2310416fd
YaeH-code <173350451+YaeH-code@users.noreply.github.com>
2026-07-06 00:55:08 +0200

    Fix: text

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,6 @@ const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "media.licdn.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "woodtrack-preprod.sos-it.fr",
-        pathname: "/**",
-      },
-    ],
   },
 };
 
